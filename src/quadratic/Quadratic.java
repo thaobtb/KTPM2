8ba0de5b29ec6346eeb4f574e6e9ff53a05a6779
unknown <sod7494@gmail.com>
2015-09-16 00:39:29 +0700

    first commit

---
+++ src/quadratic/Quadratic.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quadratic;
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Quadratic {
+
+    private double a;
+    private double b;
+    private double c;
+    public double result1;
+    public double result2;
+    public boolean check;
+    
+    public Quadratic(double a1, double b1, double c1)
+    {
+        a = a1;
+        b = b1;
+        c = c1;
+    }
+    public void calculate()
+    {
+        double delta = Math.pow(b,2)-(4*a*c);
+        if(delta < 0)
+        {
+            System.out.println("No real solutions.");
+            check = true;
+        }
+        else
+        {
+            result1 = (-b + Math.sqrt(delta))/(2*a);
+            result2 = (-b - Math.sqrt(delta))/(2*a);
+            check = false;
+            
+        }
+    }
+    public static void main(String[] args) {
+        Quadratic q = new Quadratic(1, -3, 2);
+        q.calculate();  
+        System.out.println("x1 = " + q.result1 + "\n" + "x2 = " + q.result2);
+    }
+    
+}
---
+++ test/QuadraticTest.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import quadratic.Quadratic;
+
+/**
+ *
+ * @author HP
+ */
+public class QuadraticTest {
+    
+    public QuadraticTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("Before Test");
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println("After Test");
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void NoSolutionTest()
+    {
+        Quadratic q = new Quadratic(1, -3, 2);
+        assertEquals(true, q.check);
+    }
+    
+    @Test
+    public void DoublerootTest(){
+        Quadratic q = new Quadratic(1, -2, 1);
+        assertEquals(1,q.result1,0.0);
+        assertEquals(1,q.result2,0.0);
+    }
+   
+    @Test
+    public void TwoSolutionTest()
+    {   
+        System.out.println("Two Solutions");
+        double ex1 = 2.0;
+        double ex2 = 1.0;
+        Quadratic q = new Quadratic(1, -3, 2);
+        double r1 = q.result1;
+        double r2 = q.result2;
+        assertEquals(ex1, r1, 0.0);
+        assertEquals(ex1, r2, 0.0);
+    }
+}
